0e3a14d373054119c5a9f001cb583b0fee4e45de
Gigi <gigi151521@gmail.com>
2023-10-17 20:55:40 -0600

    refactor taskSlice and editTask

--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -1,23 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = { tasks: [], editingTaskId: null };
+const initialState = { tasks: [], editingTask: null };
 
 export const taskSlice = createSlice({
   name: 'tasksState',
   initialState: initialState,
   reducers: {
     addTask: (state, action) => {
-      return state.tasks.push(action.payload);
+      const tasks = [...state.tasks];
+      tasks.push(action.payload);
+      return {
+        ...state,
+        tasks,
+      };
     },
     clearAll: (state, action) => {
       return initialState;
     },
     editTask: (state, action) => {
       const { id, description } = action.payload;
-      const foundTask = state.find((task) => task.id === id);
-      if (foundTask) {
-        foundTask.description = description;
-      }
+      const tasks = [...state.tasks];
+      const filterTasks = tasks.filter((task) => task.id !== id);
+      return {
+        editingTask: description,
+        tasks: filterTasks,
+      };
     },
   },
 });
